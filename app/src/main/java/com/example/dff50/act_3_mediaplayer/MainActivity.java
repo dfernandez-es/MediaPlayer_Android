6f1fdba60a23ace8c3d424715a63e8d6d59c07ad
Daniel <d.fernandez505@gmail.com>
2017-11-24 03:19:47 +0100

    .

--- app/src/main/java/com/example/dff50/act_3_mediaplayer/MainActivity.java
+++ app/src/main/java/com/example/dff50/act_3_mediaplayer/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.dff50.act_3_mediaplayer;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -26,6 +30,19 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        //Se solicita permisos de lectura en la memoria externa
+        ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},1);
+    }
+
+    //Se utiliza para comprobar si se tiene autorizacion para acceder a los permisos solicitados
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode==1){
+            if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+
+            }
+        }
     }
 
     @Override
@@ -41,7 +58,7 @@ public class MainActivity extends AppCompatActivity {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-        
+
 
         return super.onOptionsItemSelected(item);
     }
