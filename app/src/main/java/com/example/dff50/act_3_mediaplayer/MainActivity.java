ea110fb868061b55ec0a79b8dd8387b799ecddd8
Daniel <d.fernandez505@gmail.com>
2017-11-24 04:20:26 +0100

    .

--- app/src/main/java/com/example/dff50/act_3_mediaplayer/MainActivity.java
+++ app/src/main/java/com/example/dff50/act_3_mediaplayer/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.dff50.act_3_mediaplayer;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.os.Environment;
+import android.os.Handler;
 import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -12,8 +16,18 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.SeekBar;
+import android.widget.Toast;
+
+import java.io.File;
 
 public class MainActivity extends AppCompatActivity {
+
+    MediaPlayer mp;
+    File rutaArchivo;
+    String cancion = "master.mp3";
+    Handler h = new Handler();
+    SeekBar barra;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +35,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        barra=(SeekBar)findViewById(R.id.barra);
+
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -32,17 +48,60 @@ public class MainActivity extends AppCompatActivity {
         });
 
         //Se solicita permisos de lectura en la memoria externa
-        ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},1);
+        ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, 1);
+        rutaArchivo = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_MUSIC);
+
+        //Funciones que se ejecutan al pulsar sobre la barra
+        barra.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int prograso, boolean esUsuario) {
+                if(esUsuario){
+                    mp.seekTo(prograso);
+                }
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+                mp.pause();
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                mp.start();
+            }
+        });
     }
 
     //Se utiliza para comprobar si se tiene autorizacion para acceder a los permisos solicitados
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        if (requestCode==1){
-            if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
-
+        if (requestCode == 1) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                CargarMediaPlayer();
             }
         }
+    }
+
+    private void CargarMediaPlayer() {
+        mp = new MediaPlayer();
+        String fichero = rutaArchivo + "/" + cancion;
+        try {
+            mp.setAudioStreamType(AudioManager.STREAM_MUSIC);
+            mp.setDataSource(fichero);
+            mp.prepare();
+        } catch (Exception e) {
+            Toast.makeText(getApplicationContext(), "Error al leer el archivo", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        barra.setMax(mp.getDuration());
+        mp.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mediaPlayer) {
+                Toast.makeText(getApplicationContext(), "Cancion finalizada", Toast.LENGTH_SHORT).show();
+                mp.seekTo(0);
+                barra.setProgress(0);
+            }
+        });
     }
 
     @Override
@@ -58,8 +117,30 @@ public class MainActivity extends AppCompatActivity {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
+        if (id == R.id.play) {
+            mp.start();
+            h.postDelayed(RefrescarBarra,100);
+        }
+
+        if (id == R.id.stop) {
+            mp.pause();
+        }
 
 
         return super.onOptionsItemSelected(item);
     }
+
+    //Se encarga de actualizar la barra de progreso segun se ejecuta la cancion
+    public Runnable RefrescarBarra = new Runnable() {
+        @Override
+        public void run() {
+            //Solo se ejecuta si la musica esta en ejecucion
+            if(mp.isPlaying()) {
+                barra.setProgress((mp.getCurrentPosition()));
+                //Se llama a si misma cada 100 milisegundos
+                h.postDelayed(this, 100);
+            }
+        }
+    };
+
 }
